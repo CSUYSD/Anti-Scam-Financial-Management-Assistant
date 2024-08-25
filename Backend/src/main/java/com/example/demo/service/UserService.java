f9ed1cda111dae7dbb7d76a9f32439f93d1171e6
wchwawa <wch19961116@gmail.com>
2024-08-25 18:35:44 +1000

    Update JWT utils, 更新验证方法，用配置文件的动态密钥和时限

--- Backend/src/main/java/com/example/demo/service/UserService.java
+++ Backend/src/main/java/com/example/demo/service/UserService.java
@@ -2,82 +2,20 @@ package com.example.demo.service;
 
 import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.model.TransactionUsers;
-import com.example.demo.Dao.UserDao;
-import com.example.demo.model.UserDetail;
-import com.github.alenfive.rocketapi.entity.vo.LoginVo;
-import org.hibernate.service.spi.ServiceException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
-@Service
-public class UserService implements UserDetailsService {
-    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+public interface UserService extends UserDetailsService {
+    List<TransactionUsers> findAll();
 
+    Optional<TransactionUsers> findById(Long id);
 
-    private final UserDao userDao;
+    Optional<TransactionUsers> findByUsername(String username);
 
-    @Autowired
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
+    void updateUser(Long id, TransactionUsers updatedUser) throws UserNotFoundException;
 
-    public List<TransactionUsers> findAll() {
-        return userDao.findAll();
-    }
-
-    public Optional<TransactionUsers> findById(Long id) {
-        return userDao.findById(id);
-    }
-
-    public Optional<TransactionUsers> findByUsername(String username) {
-        return userDao.findByUsername(username);
-    }
-
-
-    public void updateUser(Long id, TransactionUsers updatedUser) throws UserNotFoundException {
-        Optional<TransactionUsers> existingUserOptional = userDao.findById(id);
-
-        if (!existingUserOptional.isPresent()) {
-            throw new UserNotFoundException("User not found");
-        }
-
-        TransactionUsers existingUser = existingUserOptional.get();
-        existingUser.setFullName(updatedUser.getFullName());
-        existingUser.setEmail(updatedUser.getEmail());
-        existingUser.setPhone(updatedUser.getPhone());
-        existingUser.setPassword(updatedUser.getPassword());
-
-        userDao.save(existingUser);
-    }
-
-    public void deleteUser(Long id) throws UserNotFoundException, DataIntegrityViolationException {
-        Optional<TransactionUsers> userOptional = userDao.findById(id);
-
-        if (!userOptional.isPresent()) {
-            throw new UserNotFoundException("User not found");
-        }
-
-        userDao.deleteById(id);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        TransactionUsers transactionUsers = userDao.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        return new UserDetail(transactionUsers, Collections.emptyList());
-    }
+    void deleteUser(Long id) throws UserNotFoundException, DataIntegrityViolationException;
 }
