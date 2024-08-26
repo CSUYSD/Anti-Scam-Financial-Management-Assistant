29bbb5f718d2db1e54196101e5b7723b21294462
wchwawa <wch19961116@gmail.com>
2024-08-26 13:59:39 +1000

    新增spring validation, model类信息校验“
    \quit
    
    /

--- Backend/src/main/java/com/example/demo/controller/AuthController.java
+++ Backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.Map;
 
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,7 @@ import com.example.demo.model.TransactionUsers;
 import com.example.demo.service.impl.AuthServiceImpl;
 import com.github.alenfive.rocketapi.entity.vo.LoginVo;
 
+@Validated
 @RestController
 public class AuthController {
 
@@ -30,19 +33,19 @@ public class AuthController {
     }
     // 登录，接收前端传来的用户名和密码，进行身份验证
     @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginVo loginVo) {
+    public ResponseEntity<Map<String, Object>> login(@Valid @RequestBody LoginVo loginVo) {
         return authServiceImpl.login(loginVo);
     }
 
     // 注册，接收前端传来的用户信息（对密码进行加密），保存到数据库
     @PostMapping("/signup")
-    public ResponseEntity<String> createUser(@RequestBody TransactionUsers transactionUsers) {
+    public ResponseEntity<String> createUser(@Valid @RequestBody TransactionUsers transactionUsers) {
         try {
             authServiceImpl.saveUser(transactionUsers);
             return ResponseEntity.status(HttpStatus.CREATED).body("用户已成功保存");
         } catch (DataIntegrityViolationException e) {
             logger.error("保存用户时出错: ", e);
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("保存用户时出错: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("用户名已存在: " + e.getMessage());
         }
     }
 }
--- Backend/src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ Backend/src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.HashMap;
 import java.util.Map;
