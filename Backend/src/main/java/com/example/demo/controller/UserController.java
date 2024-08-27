ef10134e990472856b0349e56667603f64c9eda0
wchwawa <wch19961116@gmail.com>
2024-08-27 16:07:47 +1000

    fix login bug

--- Backend/src/main/java/com/example/demo/controller/UserController.java
+++ Backend/src/main/java/com/example/demo/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.example.demo.utility.RabbitMQProducer;
 
 @RestController
 @RequestMapping("/users")
+@Validated
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
