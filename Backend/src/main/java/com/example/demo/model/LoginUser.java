92fcb8a6205e3242917805f0358b0cb97ca9d66d
feichai_fe67 <songguocheng348@gmail.com>
2024-08-27 16:07:20 +1000

    redis template

--- Backend/src/main/java/com/example/demo/model/LoginUser.java
+++ Backend/src/main/java/com/example/demo/model/LoginUser.java
@@ -5,12 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginUser implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private TransactionUsers user;
