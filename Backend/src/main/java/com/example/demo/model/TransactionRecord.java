9bfc707a6f727619167f82fa798791a1bd7c9daf
PikaVanderbilt <1657324484@qq.com>
2024-09-23 02:35:35 +1000

    增加了record的增删改查，解决了user更新密码时显示原密码不对的问题 以及添加了account的update功能，并解决了update与delete操作后redis同步问题

--- Backend/src/main/java/com/example/demo/model/TransactionRecord.java
+++ Backend/src/main/java/com/example/demo/model/TransactionRecord.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import java.time.ZonedDateTime;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,12 +18,16 @@ public class TransactionRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String type;  // Income, Expense
-    private String transactionType;    //交易类型
-    private double amount;           // 金额（元）
-    private String TransactionMethod;    // 支付方式
-    private ZonedDateTime transactionTime; // 交易时间
-    private String transactionDescription; // 交易描述
+    private String transactionType;    //
+
+    private Double amount;           //
+    private String TransactionMethod;    //
+    private ZonedDateTime transactionTime; //
+    private String transactionDescription; //
     @ManyToOne
-    @JoinColumn(name = "account_id")
+    @JoinColumn(name = "account_id", nullable = false)
+    @JsonBackReference
     private Account account; // 一个账户对应多个交易记录
+
+
 }
