7e38b0f750de35c6d613b56b5240f72fd275d273
wchwawa <wch19961116@gmail.com>
2024-10-23 17:20:43 +1100

    Upload some things

--- Backend/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,10 @@
 package com.example.demo;
 
 
+import org.springframework.ai.chroma.ChromaApi;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.openai.OpenAiEmbeddingModel;
 import org.springframework.ai.openai.api.OpenAiApi;
-import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,10 +16,15 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+import java.util.HashMap;
+import java.util.function.Consumer;
 
 @SpringBootApplication
 @EnableAsync
@@ -28,7 +32,6 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 @EnableJpaRepositories
 @EnableWebSocket
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -51,6 +54,21 @@ public class DemoApplication {
 		// Can be any other EmbeddingModel implementation.
 		return new OpenAiEmbeddingModel(new OpenAiApi(openAiApiKey));
 	}
+
+	@Value("http://localhost:8080")
+	private String ChromaBaseUrl;
+	RestClient restClient;
+	ObjectMapper objectMapper;
+
+//	@Bean
+//	public  generateChromaCollection(RestClient.Builder restClientBuilder, ObjectMapper objectMapper) {
+//		Consumer<HttpHeaders> defaultHeaders = headers -> {
+//			headers.setContentType(MediaType.APPLICATION_JSON);
+//		};
+//		this.objectMapper = objectMapper;
+//		this.restClient = restClientBuilder.baseUrl(ChromaBaseUrl).defaultHeaders(defaultHeaders).build();
+//		ChromaApi.CreateCollectionRequest request = new ChromaApi.CreateCollectionRequest("test", new HashMap<>());
+//	}
 
 
 }
